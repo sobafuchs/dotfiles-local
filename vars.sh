9b21f98348adbb33aae5b9ffb9fd4493b4d745ec
Varun Jayaraman <varun.jayaraman@gmail.com>
2018-02-28 12:49:04 -0500

    Remove knotch (#3)
    * fix merge conf
    
    * remove knotch

--- vars.sh
+++ vars.sh
@@ -3,7 +3,6 @@
 # paths
 readonly DOTFILES_PATH=$HOME/dotfiles-local
 readonly SIXPLUS_PATH=$HOME/sixplus
-readonly KNOTCH_PATH=$HOME/workspace/knotch
 
 # global vars :/
-readonly DOTFILE_PROJECTS=("knotch")
+readonly DOTFILE_PROJECTS=()
