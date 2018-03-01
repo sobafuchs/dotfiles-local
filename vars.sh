e818ebfec3cce11320eb074417c96d71f0d774d4
Varun Jayaraman <varun.jayaraman@gmail.com>
2018-03-01 19:12:45 +0100

    some interesting tweaks (#4)

--- vars.sh
+++ vars.sh
@@ -3,6 +3,7 @@
 # paths
 readonly DOTFILES_PATH=$HOME/dotfiles-local
 readonly SIXPLUS_PATH=$HOME/sixplus
+readonly LIQID_PATH=$HOME/workspace/liqid
 
 # global vars :/
-readonly DOTFILE_PROJECTS=()
+readonly DOTFILE_PROJECTS=("liqid")
