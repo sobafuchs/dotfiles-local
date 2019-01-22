153349f33bb88185a6fa9c45a9b6d790dd977fb4
varun jayaraman <varun.jayaraman@gmail.com>
2019-01-22 10:37:31 +0100

    removes unused deps

--- setup.sh
+++ setup.sh
@@ -51,18 +51,3 @@ sudo python2.7 -m ensurepip --default-pip
 
 # install awscli
  pip3 install awscli --upgrade --user
-
-# create dinghy vm using xhyve provider
-dinghy create --provider xhyve
-
-# install Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 && \
-  chmod +x minikube && \
-  sudo mv minikube /usr/local/bin/
-
-# Install hyperkit driver for minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit \
-  && chmod +x docker-machine-driver-hyperkit \
-  && sudo mv docker-machine-driver-hyperkit /usr/local/bin/ \
-  && sudo chown root:wheel /usr/local/bin/docker-machine-driver-hyperkit \
-  && sudo chmod u+s /usr/local/bin/docker-machine-driver-hyperkit
