fa5077e7144a32ffc2c10833f99d27afc8a0124e
varun jayaraman <varun.jayaraman@gmail.com>
2020-01-02 08:31:32 +0100

    Remove old projects

--- vars.sh
+++ vars.sh
@@ -6,10 +6,9 @@ export EDITOR=vi
 # paths
 DOTFILES_PATH=$HOME/dotfiles
 # SIXPLUS_PATH=$HOME/sixplus
-LIQID_PATH=$HOME/workspace/liqid
 
 # global vars :/
-DOTFILE_PROJECTS=("liqid")
+DOTFILE_PROJECTS=("")
 
 source $DOTFILES_PATH/secrets/vars.sh
 
